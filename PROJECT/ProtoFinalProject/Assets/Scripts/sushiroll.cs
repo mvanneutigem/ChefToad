209f750c7b53d025189b6d21fc3fd8198ab517a0
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2016-12-09 22:45:43 +0100

    animation test with a sushi roll :P

---
+++ PROJECT/ProtoFinalProject/Assets/Scripts/sushiroll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sushiroll : MonoBehaviour {
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("sushiroll triggered");
+        if (col.tag == "Player")
+        {
+            GetComponent<Animation>().Play();
+        }
+    }
+}
