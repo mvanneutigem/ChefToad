5b1aad8cddbd55f6044bb777853127a43ff648a9
Marieke van Neutigem <mvn882@hotmail.com>
2016-12-09 12:18:43 +0100

    lvl4 _playcontrol, basic ladders not fully functional yet

--- PROJECT/ProtoFinalProject/Assets/Scripts/PlayerController.cs
+++ PROJECT/ProtoFinalProject/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _speed = 5.0f;
     private float _rotateSpeed = 0.03f;
+    private bool _onLadder = false;
 
     //METHODS
     void Awake()
@@ -31,26 +32,52 @@ public class PlayerController : MonoBehaviour
 
             //scale camera forward to only have x and z axis to prevent character from angle-ing upwards/downwards
             var camforward = Vector3.Scale(mainCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+            
+            //ladder
+            if (_onLadder)
+            {
+                _moveVector = (vInput * Vector3.up + hInput * mainCamera.transform.right);
+            }
+            else
+            {
+                //set the movevector, relative to the camera direction
+                _moveVector = (vInput * camforward + hInput * mainCamera.transform.right);
 
-            //set the movevector, relative to the camera direction
-            _moveVector = (vInput * camforward + hInput * mainCamera.transform.right);
+            }
 
             //set the speed
             _moveVector = Speed * _moveVector.normalized;
 
-            //rotate character in the direction it's moving in
-            if (_moveVector != Vector3.zero)
+            if (!_onLadder)
             {
-                //viewvector =
-                Quaternion targetRotation = Quaternion.LookRotation(_moveVector);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.time * _rotateSpeed);
+                //rotate character in the direction it's moving in
+                if (_moveVector != Vector3.zero)
+                {
+                    //viewvector =
+                    Quaternion targetRotation = Quaternion.LookRotation(_moveVector);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.time * _rotateSpeed);
+                }
             }
         }
+
         //Gravity
         _moveVector += Physics.gravity * Time.deltaTime;
 
         //pass movement to char controller
         _characterController.Move(_moveVector * Time.deltaTime);
 
+        //ladder
+        _onLadder = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        //ladder
+        if (other.tag == "Ladder")
+        {
+            _onLadder = true;
+            Debug.Log("ladder");
+        }
     }
 }
