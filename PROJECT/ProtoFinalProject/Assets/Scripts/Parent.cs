75fd38b8799a156eb5081f00738264545e80c6d5
Marieke van Neutigem <mvn882@hotmail.com>
2016-12-09 21:24:15 +0100

    script for parenting (use for moving platforms etc) and basic working ladders (add the ladder tag to your ladders) :)

---
+++ PROJECT/ProtoFinalProject/Assets/Scripts/Parent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parent : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("parent");
+        col.transform.parent = transform.parent;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        Debug.Log("unparent");
+        col.transform.parent = null;
+    }
+}
