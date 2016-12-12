8c5f6aa847e430b894aa734efc7331fb59bc1080
Marieke van Neutigem <mvn882@hotmail.com>
2016-12-12 17:10:31 +0100

    small changes to the platform, ladder and patrol script (see slack), moving ladders in level4 now fully functional !

--- PROJECT/ProtoFinalProject/Assets/Scripts/Platform.cs
+++ PROJECT/ProtoFinalProject/Assets/Scripts/Platform.cs
@@ -9,22 +9,39 @@ public class Platform : MonoBehaviour
     public Transform[] WayPoints;
     public float Speed = 2.0f;
     private int _currentWaypoint;
+    private bool _wait = false;
+    private float _counter = 1.0f;
 
     void Update()
     {
         if (TransSelf.position.Equals(WayPoints[_currentWaypoint].position))
         {
             GoToNextWaypoint();
+            _wait = true;
         }
-        TransSelf.position = Vector3.MoveTowards(TransSelf.position, WayPoints[_currentWaypoint].transform.position, Speed * Time.deltaTime);
+        if (!_wait)
+        {
+            TransSelf.position = Vector3.MoveTowards(TransSelf.position, WayPoints[_currentWaypoint].transform.position, Speed * Time.deltaTime);
+        }
+        else
+        {
+            _counter -= Time.deltaTime;
+            if (_counter <= 0)
+            {
+                _counter = 1.0f;
+                _wait = false;
+            }
+        }
+        
     }
 
-    void GoToNextWaypoint()
+    private void GoToNextWaypoint()
     {
         ++_currentWaypoint;
         if (_currentWaypoint >= WayPoints.Length)
         {
             _currentWaypoint = 0;
         }
+        _wait = true;
     }
 }
