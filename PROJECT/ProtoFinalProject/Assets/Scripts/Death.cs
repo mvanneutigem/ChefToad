d466a01e218b69f1f9c0aae002e486782fe3b2e4
GAMEMAKER1\thoma_000 <thomas.denul@student.howest.be>
2016-12-16 12:32:47 +0100

    stuff again

---
+++ PROJECT/ProtoFinalProject/Assets/Scripts/Death.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour
+{
+    public Transform TransSpawnpoint;
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            GetComponent<Transform>().position = TransSpawnpoint.position;
+        }
+    }
+}
