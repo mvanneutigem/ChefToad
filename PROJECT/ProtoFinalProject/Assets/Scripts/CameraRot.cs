d67d803a3f7a4f381a11f2652d95ba061206c83b
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2016-12-17 22:16:33 +0100

    improved camera, use arrowkeys to move it for more of a toad feel, also slower follow and lerped.

--- PROJECT/ProtoFinalProject/Assets/Scripts/CameraRot.cs
+++ PROJECT/ProtoFinalProject/Assets/Scripts/CameraRot.cs
@@ -5,25 +5,57 @@ public class CameraRot : MonoBehaviour {
 
     //FIELDS
     public Transform Target;
-    private float _rotSpeed = 1.0f;
+    private float _rotSpeed = 10.0f;
     private Vector3 _offset;
-    public float distance = 20;
+    public float distance = 10;
+    private float _followSpeed = 5.0f;//higher is slower
     //METHODS
     // Use this for initialization
     void Start ()
     {
-        //set camera offset from target
-        _offset = new Vector3(Target.position.x + distance, Target.position.y + distance, Target.position.z + distance);
+        //set camera to target
+        _offset = new Vector3(Target.position.x, Target.position.y, Target.position.z );
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //change offset relative to mouse movement
-        _offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * _rotSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * _rotSpeed, Vector3.forward) * _offset;
-        //set camera position
-        transform.position = Target.position + _offset;
+        Debug.Log(_offset);
+        //change offset relative to input
+        ////rot axis
+        //_offset = Quaternion.AngleAxis(Input.GetAxis("CameraX") * _rotSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("CameraY") * _rotSpeed, transform.right) * _offset;
+
+        //_offset = _offset.normalized * distance;
+
+        //var nextpos = Vector3.Lerp(_offset + Target.position, transform.position, Time.deltaTime * _followSpeed);
+
+
+        //Debug.Log(nextpos);
+        //Debug.Log(_offset + Target.position);
+        ////set camera position
+        ////transform.position = nextpos;
+        //transform.position = _offset + Target.position;
         //look at target
-        transform.LookAt(Target.position);
-	}
+
+
+        //slow lookat
+        var targetLookat = Target.position - transform.position;
+        var nextlookat = Vector3.Lerp(transform.forward, targetLookat, Time.deltaTime);
+
+        transform.forward = nextlookat;
+        //transform.LookAt(Target.position);
+
+        //slow followcam
+        var spherepos = Target.position;
+        var camerapos = transform.position;
+        var camToSphere = new Vector3(spherepos.x - camerapos.x, spherepos.y - camerapos.y, spherepos.z - camerapos.z);
+        camToSphere = camToSphere.normalized;
+        camToSphere = Vector3.Lerp(camToSphere, Quaternion.AngleAxis(Input.GetAxis("CameraX") * _rotSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("CameraY") * _rotSpeed, transform.right) * camToSphere, Time.deltaTime * _rotSpeed);
+        var targetpos = new Vector3(spherepos.x - camToSphere.x * distance, spherepos.y - camToSphere.y * distance, spherepos.z - camToSphere.z * distance);
+        var targetVector = new Vector3(targetpos.x - camerapos.x, targetpos.y - camerapos.y, targetpos.z - camerapos.z) / _followSpeed;
+        var newpos = new Vector3(camerapos.x + targetVector.x, camerapos.y + targetVector.y, camerapos.z + targetVector.z);
+
+        transform.position = newpos;
+
+    }
 }
