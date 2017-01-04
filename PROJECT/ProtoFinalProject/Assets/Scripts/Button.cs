50c596fc37b789c6f046556879a0d3b6413ff9d8
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-01-04 19:34:06 +0100

    added main menu + levels menu, added camera functionality (clamping + zooming) also baked lighting level4

---
+++ PROJECT/ProtoFinalProject/Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+    //FIELDS
+
+    //METHODS
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LevelsMenu()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void loadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
--- PROJECT/ProtoFinalProject/Assets/Scripts/CameraRot.cs
+++ PROJECT/ProtoFinalProject/Assets/Scripts/CameraRot.cs
@@ -12,17 +12,18 @@ public class CameraRot : MonoBehaviour {
     private float _prevValueR = 0;
     private float _prevValueL = 0;
     private const float _rot = 45.0f;
+    private const float _angleMax = 45.0f;
+    private bool _isZoomed = false;
     //METHODS
     void Update ()
 	{
         //input
 	    var inputX = Input.GetAxis("CameraX");
         var inputY = Input.GetAxis("CameraY");
-        var zoom = Input.GetButton("Zoom");
-	    var triggerR = Input.GetButton("TriggerR");
+        var zoomup = Input.GetButtonUp("Zoom");
+        var triggerR = Input.GetButton("TriggerR");
         var triggerL = Input.GetButton("TriggerL");
-	    var prevY = transform.position.y;
-	    var prevZ = transform.position.z;
+	    float vRotation = 0f;
 
         //followcam
         var playerpos = Target.position;
@@ -38,9 +39,18 @@ public class CameraRot : MonoBehaviour {
         var nextlookat = Vector3.Lerp(transform.forward, targetLookat, _rotSpeed * Time.deltaTime);
         transform.forward = nextlookat;
 
+        //clamp cam verticalangle
+	    vRotation += _rotSpeed * inputY * Time.deltaTime;
+        var camforward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1)).normalized;
+        //making sure angle gets minus when below 0 for z axis
+	    float angleBetween = Vector3.Angle(camforward, transform.forward) * (Vector3.Cross(camforward, transform.forward).z > 0 ? -1 : 1);
+        Debug.Log(angleBetween);
+        float newAngle = Mathf.Clamp(angleBetween + vRotation, -_angleMax, _angleMax);
+	    vRotation = newAngle - angleBetween;
+
         //rotatearound
         transform.RotateAround(Target.position, Vector3.up, Time.deltaTime * _rotSpeed * inputX);
-        transform.RotateAround(Target.position, transform.right, Time.deltaTime * _rotSpeed * inputY);
+        transform.RotateAround(Target.position, transform.right, vRotation);
 
         //triggers
 	    if (triggerR)
@@ -65,58 +75,18 @@ public class CameraRot : MonoBehaviour {
             _prevValueL = 0;
         }
 
-        //clamp
-	    if (Mathf.Abs(transform.position.y - Target.position.y) > distance - 1.0f)
+        //zoom
+	    if (zoomup)
 	    {
-            Debug.Log("fix");
-            transform.position = new Vector3(transform.position.x, prevY, prevZ);
+	        if (!_isZoomed)
+	        {
+	            distance *= 0.5f;
+            }
+	        else
+	        {
+                distance *= 2.0f;
+            }
+	        _isZoomed = !_isZoomed;
 	    }
-
-        //-------------------------------------------------------------
-        //i fucked up so i started over ^^
-        //Debug.Log(_offset);
-        //change offset relative to input
-        ////rot axis
-        //_offset = Quaternion.AngleAxis(Input.GetAxis("CameraX") * _rotSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("CameraY") * _rotSpeed, transform.right) * _offset;
-
-        //_offset = _offset.normalized * distance;
-
-        //var nextpos = Vector3.Lerp(_offset + Target.position, transform.position, Time.deltaTime * _followSpeed);
-
-
-        //Debug.Log(nextpos);
-        //Debug.Log(_offset + Target.position);
-        ////set camera position
-        ////transform.position = nextpos;
-        //transform.position = _offset + Target.position;
-        //look at target
-
-
-        //slow lookat
-        //var targetLookat = Target.position - transform.position;
-        //var nextlookat = Vector3.Lerp(transform.forward, targetLookat, Time.deltaTime);
-        //transform.forward = nextlookat;
-
-        //lookat
-        //transform.LookAt(Target.position);
-
-        //slow followcam
-        //var spherepos = Target.position;
-        //var camerapos = transform.position;
-        //var camToSphere = new Vector3(spherepos.x - camerapos.x, spherepos.y - camerapos.y, spherepos.z - camerapos.z);
-        //camToSphere = camToSphere.normalized;
-        ////camToSphere = Vector3.Lerp(camToSphere, Quaternion.AngleAxis(Input.GetAxis("CameraX") * _rotSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("CameraY") * _rotSpeed, transform.right) * camToSphere, Time.deltaTime * _rotSpeed);
-        //var targetpos = new Vector3(spherepos.x - camToSphere.x * distance, spherepos.y - camToSphere.y * distance, spherepos.z - camToSphere.z * distance);
-        //var targetVector = new Vector3(targetpos.x - camerapos.x, targetpos.y - camerapos.y, targetpos.z - camerapos.z) / _followSpeed;
-        //var newpos = new Vector3(camerapos.x + targetVector.x, camerapos.y + targetVector.y, camerapos.z + targetVector.z);
-
-        ////if (Mathf.Abs(newpos.y - Target.transform.position.y) > distance - 1.0f)
-        ////{
-        ////    newpos.y = distance - 1.0f;
-        ////}
-
-        //transform.position = newpos;
-
-
     }
 }
