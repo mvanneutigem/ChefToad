cc61a9eec68c8b466a933e6a5cd53f43f87ca1fc
GAMEMAKER1\thoma_000 <thomas.denul@student.howest.be>
2017-01-29 16:55:30 +0100

    Start game skips level one

--- PROJECT/ProtoFinalProject/Assets/Scripts/GameController.cs
+++ PROJECT/ProtoFinalProject/Assets/Scripts/GameController.cs
@@ -21,7 +21,7 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        if (SceneManager.GetActiveScene().buildIndex != 1)
+        if (SceneManager.GetActiveScene().buildIndex != 2)
         {
             _toadalLives = PlayerPrefs.GetInt("ToadalLives");
             score = PlayerPrefs.GetInt("Score");
