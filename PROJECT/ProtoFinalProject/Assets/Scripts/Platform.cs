de192c10826b1d5569e4b9cf9906b3d7e0336f2b
Marieke van Neutigem <marieke.van.neutigem@student.howest.be>
2017-01-29 11:25:09 +0100

    Changed the ladderSpeed and pauseTime (feel free to test if it's better) made the ingredientUI screen res independent, so ull have to drag to the canvas again :X

--- PROJECT/ProtoFinalProject/Assets/Scripts/Platform.cs
+++ PROJECT/ProtoFinalProject/Assets/Scripts/Platform.cs
@@ -11,9 +11,14 @@ public class Platform : MonoBehaviour
     public float Speed = 2.0f;
     private int _currentWaypoint;
     private bool _wait = false;
-    private float _counter = 1.0f;
+    public float WaitTime = 1.0f;
+    private float _counter;
 
     //METHODS
+    void Start()
+    {
+        _counter = WaitTime;
+    }
     void Update()
     {
         if (TransSelf.position.Equals(WayPoints[_currentWaypoint].position))
@@ -37,7 +42,7 @@ public class Platform : MonoBehaviour
                 _counter -= Time.deltaTime;
                 if (_counter <= 0)
                 {
-                    _counter = 1.0f;
+                    _counter = WaitTime;
                     _wait = false;
                 }
             }
